c7705eee97d7010d4d67f3767a224f896a0284ff
nwachim12 <nwachim12@gmail.com>
2024-07-30 20:10:20 -0400

    Initial Commit

---
+++ Reminder/ReminderApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReminderApp.swift
+//  Reminder
+//
+//  Created by Michael Nwachi on 7/30/24.
+//
+
+import SwiftUI
+
+@main
+struct ReminderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
